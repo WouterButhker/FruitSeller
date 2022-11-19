12e9b7cbc438df442386dfc9cee66c278197abb9
Wouter Büthker <w.w.buthker@student.tudelft.nl>
2022-11-19 11:43:40 +0100

    Initial commit

---
+++ src/main/kotlin/furhatos/app/fruitseller/flow/main/greeting.kt
@@ -0,0 +1,20 @@
+package furhatos.app.fruitseller.flow.main
+
+import furhatos.app.fruitseller.flow.Parent
+import furhatos.flow.kotlin.*
+import furhatos.nlu.common.No
+import furhatos.nlu.common.Yes
+
+val Greeting : State = state(Parent) {
+    onEntry {
+        furhat.ask("Should I say Hello World?")
+    }
+
+    onResponse<Yes> {
+        furhat.say("Hello World! .")
+    }
+
+    onResponse<No> {
+        furhat.say("Ok.")
+    }
+}
